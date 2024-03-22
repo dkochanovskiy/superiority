f517923634c423d2e55db4c31cb118b0a175c18a
dkochanovskiy <dkochanovskiy@gmail.com>
2024-03-22 13:01:57 +0300

    refresh every half minutes

--- src/main/java/com/company/superiority/screen/planin/PlaninBrowse.java
+++ src/main/java/com/company/superiority/screen/planin/PlaninBrowse.java
@@ -1,15 +1,34 @@
 package com.company.superiority.screen.planin;
 
+import io.jmix.ui.Notifications;
+import io.jmix.ui.action.Action;
 import io.jmix.ui.component.Button;
+import io.jmix.ui.component.Timer;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import com.company.superiority.entity.Planin;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @UiController("Planin.browse")
 @UiDescriptor("planin-browse.xml")
 @LookupComponent("planinsTable")
 public class PlaninBrowse extends StandardLookup<Planin> {
-    @Subscribe("createBtn")
-    public void onCreateBtnClick(final Button.ClickEvent event) {
-        
+    @Autowired
+    private CollectionLoader<Planin> planinsDl;
+
+    @Autowired
+    protected Timer refresh;
+
+    @Autowired
+    protected Notifications notifications;
+
+    @Subscribe("refresh")
+    protected void onTimerTick(Timer.TimerActionEvent event) {
+        planinsDl.load();
+        notifications.create(Notifications.NotificationType.TRAY)
+                .withCaption("Данные обновлены")
+                .show();
     }
 }
